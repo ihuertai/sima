4da6af7ca699980ac0cebb34585ad715a0697fa9
ihuertai <80089961+ihuertai@users.noreply.github.com>
2026-03-02 11:30:57 -0700

    primer envio

---
+++ src/main/java/mx/ipn/sima/SimaApplication.java
@@ -0,0 +1,13 @@
+package mx.ipn.sima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimaApplication.class, args);
+	}
+
+}
